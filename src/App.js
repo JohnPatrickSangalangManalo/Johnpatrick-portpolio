1a7a8b4dd25ba28ebc3bb97f1125e443df8db0e2
John Patrick <manalojohnpatricksangalang@gmail.com>
2025-08-23 18:25:37 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import './App.css';
+import './components/css/App.css';
 
 function App() {
   const [loading, setLoading] = useState(true);
